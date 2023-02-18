7720d5c843c334348878e20920693c9ddf1387c7
Hopsaheysa <stranskymarek90@gmail.com>
2023-02-18 12:58:36 +0100

    Delivers items array on 200 HTTPResponse with JSON items

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -8,9 +8,27 @@
 import Foundation
 
 public struct FeedItem: Equatable {
+    // should i add  public to preporties???
     let id: UUID
     let description: String?
     let location: String?
     let imageURL: URL
+    
+    // in video this is also public
+    init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
 	
