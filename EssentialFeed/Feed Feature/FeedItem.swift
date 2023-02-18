c7466eace7dadabac4cc58bee543b47d37cf2f47
Hopsaheysa <stranskymarek90@gmail.com>
2023-02-18 13:43:07 +0100

    Move Decodable logic to a new private Item struct to decouple the Feed Feature module from API implementation details

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -8,27 +8,15 @@
 import Foundation
 
 public struct FeedItem: Equatable {
-    // should i add  public to preporties???
-    let id: UUID
-    let description: String?
-    let location: String?
-    let imageURL: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
     
-    // in video this is also public
-    init(id: UUID, description: String?, location: String?, imageURL: URL) {
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
         self.id = id
         self.description = description
         self.location = location
         self.imageURL = imageURL
     }
 }
-
-extension FeedItem: Decodable {
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case description
-        case location
-        case imageURL = "image"
-    }
-}
-	
