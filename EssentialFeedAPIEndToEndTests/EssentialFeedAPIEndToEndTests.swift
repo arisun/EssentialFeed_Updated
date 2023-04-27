c5d1ad56b645adc3e4e4da74d108af93cbce5f03
Hopsaheysa <stranskymarek90@gmail.com>
2023-04-27 10:19:04 +0200

    Add Travis CI config

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -51,7 +51,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             receivedResult = result
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 8)
+        wait(for: [exp], timeout: 20)
         
         return receivedResult
     }
